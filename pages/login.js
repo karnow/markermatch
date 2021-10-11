6a1034d802ba06ff734c9e95e0046f32294b999a
Karol Nowakowski <karolnowakowski@iMac-Carl.local>
2021-10-11 18:26:53 +0200

    corrections and addition of logic in layaut and login files

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,26 @@
+import { signIn } from 'next-auth/client';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
 
 export default function Login() {
+  const [signing, setSigning] = useState(false);
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  const handleGithubLogin = async (e) => {
+    e.preventDefault();
+    setSigning(true);
+    await signIn('github');
+  };
+
+  useEffect(() => {
+    if (session && !loading) {
+      router.push('/');
+    }
+  }, [session, loading]);
+
   return (
     <div>
       <section className="h-screen py-10 lg:py-20 bg-green-600">
@@ -16,16 +36,17 @@ export default function Login() {
                 <span className="text-gray-500">Sign In</span>
                 <h3 className="text-2xl font-bold">Join our community</h3>
               </div>
-              <form action="">
+              <form>
                 <div className="text-center">
-                  <a
-                    className="mt-8 mb-4 p-4 flex justify-center items-center border rounded hover:bg-gray-50"
-                    href="/">
+                  <button
+                    className="mt-8 mb-4 p-4 w-full flex justify-center items-center border rounded hover:bg-gray-50"
+                    onClick={handleGithubLogin}>
                     <img className="mr-4 w-6" src="/github.svg" alt="" />
                     <span className="text-xs text-gray-500 font-bold">
-                      Sign In with your GitHub
+                      {!signing && 'Sign in with your GitHub'}
+                      {signing && 'Signing in...'}
                     </span>
-                  </a>
+                  </button>
                 </div>
               </form>
             </div>
